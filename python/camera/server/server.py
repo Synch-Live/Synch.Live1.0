d88c5f000d56ef7ef1203dbd5974024a45566841
ay322 <anastasia.yurevich22@imperial.ac.uk>
2023-02-04 19:36:10 +0000

    fixed ansible path

--- python/camera/server/server.py
+++ python/camera/server/server.py
@@ -27,6 +27,8 @@ awb_modes = [
     "greyworld"
 ]
 
+ansible_dir_path = "../ansible"
+
 def create_app(server_type, conf, conf_path, camera_stream=None):
     app = Flask(__name__)
     app.debug = True
@@ -59,21 +61,21 @@ def create_app(server_type, conf, conf_path, camera_stream=None):
 
     @app.route("/run_ansible_set_up_scripts")
     def run_ansible_script():
-        r = ansible_runner.run(private_data_dir='./ansible', playbook = 'config_hardware.yml', forks=10, status_handler = my_status_handler)
-        r = ansible_runner.run(private_data_dir='./ansible', playbook = 'install_software.yml', forks = 10, status_handler = my_status_handler)
-        r = ansible_runner.run(private_data_dir='./ansible', playbook = 'sync_time.yml', forks = 10, status_handler = my_status_handler)
-        r = ansible_runner.run(private_data_dir='./ansible', playbook = 'reboot.yml', forks = 10, cmdline = '-- tags reboot', status_handler = my_status_handler)
-        r = ansible_runner.run(private_data_dir='./ansible', playbook = 'sync_code.yml', forks = 10, status_handler = my_status_handler)
+        r = ansible_runner.run(private_data_dir=ansible_dir_path, playbook = 'config_hardware.yml', forks=10, status_handler = my_status_handler)
+        r = ansible_runner.run(private_data_dir=ansible_dir_path, playbook = 'install_software.yml', forks = 10, status_handler = my_status_handler)
+        r = ansible_runner.run(private_data_dir=ansible_dir_path, playbook = 'sync_time.yml', forks = 10, status_handler = my_status_handler)
+        r = ansible_runner.run(private_data_dir=ansible_dir_path, playbook = 'reboot.yml', forks = 10, cmdline = '-- tags reboot', status_handler = my_status_handler)
+        r = ansible_runner.run(private_data_dir=ansible_dir_path, playbook = 'sync_code.yml', forks = 10, status_handler = my_status_handler)
         return "Running anible set up scripts"
 
     @app.route("/run_ansible_to_copy_latest_python_files")
     def run_ansible_to_copy_latest_python_files():
-        r = ansible_runner.run(private_data_dir='./ansible', playbook = 'sync_code.yml', forks = 10, status_handler = my_status_handler)
+        r = ansible_runner.run(private_data_dir=ansible_dir_path, playbook = 'sync_code.yml', forks = 10, status_handler = my_status_handler)
         return "Running anible script that copies the latest python files"
 
     @app.route("/run_ansible_script_to_synchronise_clocks")
     def run_ansible_script_to_synchronise_clocks():
-        r = ansible_runner.run(private_data_dir='./ansible', playbook = 'sync_time.yml', forks = 10, cmdline = '--tags experiment', status_handler = my_status_handler)
+        r = ansible_runner.run(private_data_dir=ansible_dir_path, playbook = 'sync_time.yml', forks = 10, cmdline = '--tags experiment', status_handler = my_status_handler)
         return "Running anible script to synchronise clocks for experiment"
 
     def my_status_handler(data, runner_config):
